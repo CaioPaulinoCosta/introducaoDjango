35d1416e5b45e81bf9dc19b096f352d0f496ce96
Caio Costa <caiopaulinocosta@hotmail.com>
2024-03-05 12:47:44 +0000

    primeiro commit

---
+++ projetodjango/virtual/bin/sum_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def sum_numbers(request):
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    result = None
+
+    if num1 and num2:
+        result = int(num1) + int(num2)
+
+    return render(request, 'result.html', {'num1': num1, 'num2': num2, 'result': result})
